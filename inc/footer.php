4c2f5b4511b4ecd939072a8ddf8190d5db7c899f
KIRAN KUMAR ROUT <routkiran04@gmail.com>
2024-08-29 22:42:43 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Team L&FWA</span></strong>. All Rights Reserved by Kripa, Kiran, Bittu
+    </div>
+  </footer><!-- End Footer -->
+
+   
+<!-- Vendor JS Files -->
+<script src="<?= base_url ?>assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="<?= base_url ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url ?>assets/vendor/chart.js/chart.umd.js"></script>
+<script src="<?= base_url ?>assets/vendor/echarts/echarts.min.js"></script>
+<script src="<?= base_url ?>assets/vendor/quill/quill.min.js"></script>
+<script src="<?= base_url ?>assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="<?= base_url ?>assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="<?= base_url ?>assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="<?= base_url ?>assets/js/main.js"></script>
+<svg width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;"><defs id="SvgjsDefs1002"></defs><polyline id="SvgjsPolyline1003" points="0,0"></polyline><path id="SvgjsPath1004" d="M0 0 "></path></svg>
+  <!-- <div style="top: 4.5em; position: fixed; right: unset; width: auto; opacity: 1; z-index:100000000000; left: -1.5em;"><a href="mailto:teamlfis@gmail.com" style="color: rgb(126, 126, 126); font-weight: bolder; background: rgb(235, 235, 235); padding: 1em 3em; border-radius: 50px; text-decoration: unset; font-size: 11px;">Developed By Team Lost And Found</a></div> -->
+  <div style="top: 4.5em; position: fixed; left: unset; width: auto; opacity: 1; z-index: 10000000000; right: -1.5em;"><a href="mailto:teamlfis@gmail.com" style="color: rgb(126, 126, 126); font-weight: bolder; background: rgb(235, 235, 235); padding: 1em 3em; border-radius: 50px; text-decoration: unset; font-size: 11px;">Welcome to Lost and Found Application</a></div>
